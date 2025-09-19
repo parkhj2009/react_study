f78df88af0ffe5c829e9088102d92276c78896a6
parkhj2009 <eee0110010100@gmail.com>
2025-09-19 14:19:30 +0900

    마커 여러개 생성

--- map-test/src/App.jsx
+++ map-test/src/App.jsx
@@ -8,20 +8,12 @@ function App() {
   const kakaoKey = import.meta.env.VITE_APP_KEY;
   const mapRef = useRef(null);
   const mapInstance = useRef(null);
+  const [coords, setCoords] = useState("지도에서 위치를 클릭하면 좌표가 표시됩니다.");
 
   useEffect(() => {
 
     if (mapInstance.current) return;
 
-    if (!window.kakao) {
-      const script = document.createElement('script');
-      script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${kakaoKey}&autoload=false`;
-      script.async = true;
-      script.onload = () => initMap();
-      document.head.appendChild(script);
-    } else {
-      initMap();
-    }
 
     function initMap() {
       window.kakao.maps.load(() => {
@@ -34,11 +26,23 @@ function App() {
                 level: 1
               };
 
-            mapInstance.current = new window.kakao.maps.Map(mapRef.current, mapOptions);
+              const map = new window.kakao.maps.Map(mapRef.current, mapOptions);
 
-              new window.kakao.maps.Marker({
-                position: new window.kakao.maps.LatLng(latitude, longitude),
-                map: mapInstance.current,
+              const imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png'
+              const imageSize = new kakao.maps.Size(64, 69) // 마커이미지의 크기입니다
+              const imageOption = { offset: new kakao.maps.Point(27, 69) };
+              const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+
+              window.kakao.maps.event.addListener(map, 'click', function (mouseEvent) {
+                const latlng = mouseEvent.latLng;
+
+                new window.kakao.maps.Marker({
+                  position: latlng,
+                  image: markerImage,
+                  map: map,
+                });
+
+                setCoords(`클릭한 위치의 위도는 ${latlng.getLat().toFixed(2)} 이고, 경도는 ${latlng.getLng().toFixed(2)} 입니다`);
               });
             },
             (error) => {
@@ -50,11 +54,21 @@ function App() {
         }
       });
     }
+    if (!window.kakao) {
+      const script = document.createElement('script');
+      script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${kakaoKey}&autoload=false`;
+      script.async = true;
+      script.onload = () => initMap();
+      document.head.appendChild(script);
+    } else {
+      initMap();
+    }
   }, []);
   return (
     <div>
-      <h1>카카오맵</h1>
-      <div ref={mapRef} style={{ width: '402px', height: '820px' }}></div>
+      <h2>카카오맵</h2>
+      <div ref={mapRef} style={{ width: '400px', height: '650px' }}></div>
+      <p>{coords}</p>
     </div>
   );
 }
